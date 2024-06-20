eb24aa49c5caed397e7621ddbb88e354ce0f5622
Leon Dickens <leon.dickens@hotmail.com>
2024-06-20 12:13:17 +0100

    init

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char[,] viewportArray = new char[15, 108];
+
+for (int x = 0; x < 15; x++)
+{
+    for (int y = 0; y < 108; y++)
+    {
+        Console.Write("a");
+    }
+    Console.WriteLine("\n");
+}
